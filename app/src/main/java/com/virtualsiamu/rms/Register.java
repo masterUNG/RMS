6a2b595b2c4be663fd46785430ea5d0cc80127ea
masterUNG <phrombutr@gmail.com>
2016-08-25 15:54:51 +0700

    Read JSON finish

--- app/src/main/java/com/virtualsiamu/rms/Register.java
+++ app/src/main/java/com/virtualsiamu/rms/Register.java
@@ -1,10 +1,17 @@
 package com.virtualsiamu.rms;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class Register extends AppCompatActivity {
 
@@ -16,6 +23,7 @@ public class Register extends AppCompatActivity {
     private String nameIDString, nameSurString,
             codeMString, emailString,
             userString, passwordString;
+    private String[] majorStrings;
 
 
     @Override
@@ -31,7 +39,53 @@ public class Register extends AppCompatActivity {
         passwordEditText = (EditText) findViewById(R.id.editText6);
         codeMSpinner = (Spinner) findViewById(R.id.editText3);
 
+        SynMajorTABLE synMajorTABLE = new SynMajorTABLE(this, codeMSpinner);
+        synMajorTABLE.execute();
+
     }   // Main Method
+
+    //Create Inner Class
+    private class SynMajorTABLE extends AsyncTask<Void, Void, String> {
+
+        // Explicit
+        private Context context;
+        private Spinner mySpinner;
+        private static final String urlJSON = "http://virtualsiamu.com/RMS/Major/Get_Major.php";
+
+        public SynMajorTABLE(Context context, Spinner mySpinner) {
+            this.context = context;
+            this.mySpinner = mySpinner;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("25AugV1", "e doinback ==> " + e.toString());
+                return null;
+            }
+
+
+        }   // doIn
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("25AugV1", "JSON ==> " + s);
+
+        }   // onPost
+
+    }   // SynClass
+
 
     public void clickSignUpRegis(View view) {
 
